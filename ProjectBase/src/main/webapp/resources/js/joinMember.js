72fb8a638770f67d3bdf45269eb79d8bf4f4c165
DESKTOP-9BUORIG\user <sungyeol31@gmail.com>
2020-09-28 17:29:36 +0900

    200928 박성열-4

--- ProjectBase/src/main/webapp/resources/js/joinMember.js
+++ ProjectBase/src/main/webapp/resources/js/joinMember.js
@@ -1,7 +1,5 @@
 /* 페이지 로딩 후 실행 */
 $(document).ready(function(){
-	alert('joinMember.js 로딩 성공');
-	
 	//클릭 이벤트
 	//$(document).on('click', '선택자', function() {
 
@@ -167,6 +165,10 @@ $(document).ready(function(){
           // $( "#dialog-confirm" ).dialog( "open" );
 		}
 	});
+	
+	$(document).on('click', '.choiceAddr', function() {
+		$('#memberAddr1').val($(this).text());
+	});
 });
 
 /* 함수선언 영역*/
@@ -175,4 +177,93 @@ $(document).ready(function(){
 	//aaa = function(){
 	
 	//};
+	getAddrLoc = function(){
+		// 적용예 (api 호출 전에 검색어 체크) 	
+		if (!checkSearchedWord(document.form.keyword)) {
+			return ;
+		}
+
+		$.ajax({
+			 url :"getAddrApi.do"
+			,type:"post"
+			,data:$("#form").serialize()
+			,dataType:"json"
+			,success:function(jsonStr){
+				$("#list").html("");
+				var errCode = jsonStr.results.common.errorCode;
+				var errDesc = jsonStr.results.common.errorMessage;
+				if(errCode != "0"){
+					alert(errCode+"="+errDesc);
+				}else{
+					if(jsonStr != null){
+						makeListJson(jsonStr);
+					}
+				}
+			}
+		    ,error: function(xhr,status, error){
+		    	alert("에러발생");
+		    }
+		});
+	}
+	
+	makeListJson = function(jsonStr){
+		var totalCnt = jsonStr.results.common.totalCount
+		var htmlStr = totalCnt;
+		htmlStr += "<table>";
+		$(jsonStr.results.juso).each(function(){
+			htmlStr += "<tr>";
+			htmlStr += "<td>"+"<span class='choiceAddr'>"+this.zipNo;
+			htmlStr += " "+this.roadAddrPart1;
+			htmlStr += " ("+this.jibunAddr+")"+"</span>"+"</td>";
+		});
+		htmlStr += "</table>";
+		if (totalCnt > 100) {
+			pageCnt = 10;
+		} else {
+			pageCnt = Math.ceil(totalCnt/10);
+		}
+		htmlStr += totalCnt/10;
+		htmlStr += " " + pageCnt;
+		$("#list").html(htmlStr);
+	}
+	
+	//특수문자, 특정문자열(sql예약어의 앞뒤공백포함) 제거
+	checkSearchedWord = function(obj){
+		if(obj.value.length >0){
+			//특수문자 제거
+			var expText = /[%=><]/ ;
+			if(expText.test(obj.value) == true){
+				alert("특수문자를 입력 할수 없습니다.") ;
+				obj.value = obj.value.split(expText).join(""); 
+				return false;
+			}
+			
+			//특정문자열(sql예약어의 앞뒤공백포함) 제거
+			var sqlArray = new Array(
+				//sql 예약어
+				"OR", "SELECT", "INSERT", "DELETE", "UPDATE", "CREATE", "DROP", "EXEC",
+	             		 "UNION",  "FETCH", "DECLARE", "TRUNCATE" 
+			);
+			
+			var regex;
+			for(var i=0; i<sqlArray.length; i++){
+				regex = new RegExp( sqlArray[i] ,"gi") ;
+				
+				if (regex.test(obj.value) ) {
+				    alert("\"" + sqlArray[i]+"\"와(과) 같은 특정문자로 검색할 수 없습니다.");
+					obj.value =obj.value.replace(regex, "");
+					return false;
+				}
+			}
+		}
+		return true ;
+	}
+	
+	enterSearch = function() {
+		var evt_code = (window.netscape) ? ev.which : event.keyCode;
+		if (evt_code == 13) {    
+			event.keyCode = 0;  
+			getAddrLoc(); 
+		} 
+	}
 })(jQuery);
